7ccd85ad568edc714b9dfcd41536e0c8a4e76675
Aislinn Schepers <Aislinn Schepers>
2016-02-06 14:15:10 -0500

    Beginning of rest of Autonomous Code

--- org/usfirst/frc/team435/robot/automodes/ReachDefense.java
+++ org/usfirst/frc/team435/robot/automodes/ReachDefense.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team435.robot.automodes;
 
 import org.usfirst.frc.team435.robot.commands.DriveFoward;
-import org.usfirst.frc.team435.robot.commands.TurnLeft;
-import org.usfirst.frc.team435.robot.commands.TurnRight;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,9 +11,6 @@ public class ReachDefense extends CommandGroup {
     
     public  ReachDefense() {
         addSequential(new DriveFoward(1, 2));
-        addSequential(new TurnLeft(.5, 1));
-        addSequential(new DriveFoward(1, 2));
-        addSequential(new TurnRight(.5, 1));
-        addSequential(new DriveFoward(1, 5));
+        
     }
 }
